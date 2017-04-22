9ddcb1ec591c2edd9f3195f2caaf4117feae10fe
Dan <rudwebb2014@gmail.com>
2017-04-22 15:02:23 -0400

    Updating to use picture that does not have background remved

--- VideoClip&CircleDetect/GetOFExample.m
+++ VideoClip&CircleDetect/GetOFExample.m
@@ -1,12 +1,14 @@
 LeftVideo = 'left_shot3.mp4';
 RightVideo = 'right_shot3.mp4';
-SpaceBetweenFrames = 2;
+SpaceBetweenFrames = 1;
 NumOfFrames = 3;
-BegIndxFrame = 4;
+BegIndxFrame = 8;
 
-[Lc, Rc, Lr, Rr, Li_orig, Ri_orig, Li_edited, Ri_edited , ROFVx, ROFVy , LOFVx, LOFVy] = GetOpticalFlowFromVideo(LeftVideo, RightVideo,SpaceBetweenFrames, NumOfFrames, BegIndxFrame);
+[Lc, Rc, Lr, Rr, Li_orig, Ri_orig, Li_edited, Ri_edited , ROFVx, ROFVy , LOFVx, LOFVy, flowR, flowL] = GetOpticalFlowFromVideo(LeftVideo, RightVideo,SpaceBetweenFrames, NumOfFrames, BegIndxFrame);
 
-image = cell2mat(Ri_edited(1));
+image = cell2mat(Ri_orig(3));
 imshow(image);
-TestVelx = cell2mat(ROFVx(1));
-center = cell2mat(Lc(1));
+hold on;
+TestVelx = cell2mat(ROFVx(2));
+center = cell2mat(Rc(2));
+plot(flowR,'DecimationFactor',[2 2],'ScaleFactor',7)
