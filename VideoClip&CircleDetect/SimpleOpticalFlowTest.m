68ed13320218b54779df0f4bbb1ead3c97b28a1a
Dan <rudwebb2014@gmail.com>
2017-04-22 21:59:37 -0400

    Updating to farneback method with smoothing

---
+++ VideoClip&CircleDetect/SimpleOpticalFlowTest.m
@@ -0,0 +1,18 @@
+numFrames = 6;
+startFrame = 13;
+video = VideoReader('right_shot3.mp4');
+frames = cell(numFrames, 1);
+lastFrame = startFrame + numFrames -1;
+index = 1;
+for i = startFrame: lastFrame
+    frames{index} = read(video,i);
+    index = index + 1;
+end
+
+
+[flow, VelX, VelY] = GetVelocityMatrixOpticalFlow( frames );
+
+image = cell2mat(frames(numFrames));
+imshow(image);
+hold on;
+plot(flow,'DecimationFactor',[3 3],'ScaleFactor',2)
