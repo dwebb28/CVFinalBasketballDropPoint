2826552355de92530f886a63d18e7c7a5efe668c
zjthangz <zhujintian_93@hotmail.com>
2017-04-30 14:55:33 -0400

    Add files via upload

--- TrajecoryFromPosition3d.m
+++ TrajecoryFromPosition3d.m
@@ -1,20 +1,35 @@
-function [resp,resr] = TrajecoryFromPosition3d(pcell, alfa, beta, rho, u0, v0, f, D, boardx)
+function [resp,resr] = TrajecoryFromPosition3d(pcell,r)
 %p: n*2 matrix representing n points;
-%alfa, beta, rho, u0, v0: intrinsic parameters
+%alpha, beta, rho, u0, v0: intrinsic parameters
+alpha = 1.364814621127952e+03;
+beta = 1.368750434258315e+03;
+thet = 89.482506651068330;
+u0 = 6.256611682184122e+02;
+v0 = 3.527864605555271e+02;
 %f: focal length
+f = 4.15;
 %D: actual diameter of basketball;
+D = 724;
 %further needed: y coordinate of hoop; x coordinate of backboard
+%boardz = 16000;
+
 n=size(pcell);
 n=n(1);
+
 p=cell2mat(pcell);
 
-K=[alfa -alfa*cot(rho) u0; %need intrinsic parameter
-    0 beta/sin(rho) v0;
+K=[alpha -alpha*cot(thet) u0; %need intrinsic parameter
+    0 beta/sin(thet) v0;
     0 0 1];
 p = [p,ones(n,1)];
-pc = (K\f*p')';%coordinate in camera frame
+pc = (K\(f*p'))';%coordinate in camera frame
+rc = r*2/(alpha+beta);%ball radius pixels->length in in camera frame(x)
 
-z=D*f./(2*r);%[n,1]
+t=1:n;
+rfit = polyfit(t,rc',2);
+rc = rfit(1)*t'.^2+rfit(2)*t'+rfit(3);
+
+z=D*f./(2*rc);%[n,1]
 x=pc(:,1).*z/f;
 y=pc(:,2).*z/f;
 X=x+pc(:,1);
@@ -37,15 +52,18 @@ pnewSet=pnew;
 newdX=dX(n-1);
 newdY=dY(n-1);
 newdZ=dZ(n-1);
-while pnew(1)>boardx %temporary, need consider hoop
-    newdX=newdX*(1-k);
-    newdZ=newdZ*(1-k);
+count = 1;
+while pnew(2)<0 %temporary, need consider hoop
+    newdX=newdX*(1-kX);
+    newdZ=newdZ*(1-kZ);
     newdY=newdY*(1-k)-a;
     pnew=[pnew(1)+newdX, pnew(2)+newdY, pnew(3)+newdZ];
     pnewSet=[pnewSet;pnew];
+    count = count+1;
 end
-
-unew=pnewSet(:,1)./(1+(pnewSet(:,3)-f)/f);
-vnew=pnewSet(:,2)./(1+(pnewSet(:,3)-f)/f);
-resp=[unew,vnew];
-resr=f*D./2*(pnewSet(:,3)-f);
+xnew=pnewSet(:,1)*f./pnewSet(:,3);
+ynew=pnewSet(:,2)*f./pnewSet(:,3);
+pxnewSet = [xnew/f,ynew/f,ones(count,1)];
+resp = (K*(pxnewSet'))';
+rnew = D*f./(2*pnewSet(:,3));
+resr = rnew * (alpha+beta)/2;
